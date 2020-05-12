65ecfb5c631d51d26a7cfa87f91b510cb712710b
farhad <farhadhossain0085@gmail.com>
2020-05-12 16:35:36 +0600

    Links file added

--- grabemail/grabemail/spiders/emailbot.py
+++ grabemail/grabemail/spiders/emailbot.py
@@ -1,38 +1,30 @@
 import scrapy
 import re
 from ..items import GrabemailItem
+from ..websitelinks import linkHelpMan
 
 class EmailbotSpider(scrapy.Spider):
     name = 'emailbot'
+    link = linkHelpMan()
 
-    #allowed_domains = ["localhost"]
+    start_urls = link.getFirstLink()
 
-    start_urls = [
-        'https://detailed.com/50/'
-    ]
-    i = 0
-
+    i = 2
 
     def parse(self, response):
+
         items = GrabemailItem()
 
-        urls = response.css("a::attr(href)").extract()
         reobj = re.compile(r"\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,6}\b", re.IGNORECASE)
         emailList = re.findall(reobj, response.css("html")[0].extract())
 
         items['emailList'] = emailList
 
-        for url in urls:
-            print(str(self.i) + " urls checked")
-            if self.i>200:
-                break
-            if url.startswith("mailto") or \
-                    url.startswith("http://account") or \
-                    url.startswith("https://account") or\
-                    url.startswith("http://accounts") or \
-                    url.startswith("https://accounts"):
-                continue
+        for l in range(self.link.linkNumber()):
+
+            url = self.link.readExactLink(self.i)
+            self.i += 1
+
             yield response.follow(url, callback=self.parse)
-            self.i +=1
 
         yield items
