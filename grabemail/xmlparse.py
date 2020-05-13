d69ef9f2c89be2eb8eadc8972f9b80433c867cd0
farhad <farhadhossain0085@gmail.com>
2020-05-13 23:40:12 +0600

    updated

--- grabemail/xmlparse.py
+++ grabemail/xmlparse.py
@@ -4,7 +4,10 @@ import sqlite3 as db
 conn = db.connect("emaildb.db")
 curr = conn.cursor()
 
-curr.execute("create table 'emails'")
+curr.execute("""CREATE TABLE IF NOT EXISTS "emails" (
+	"id"	INTEGER PRIMARY KEY AUTOINCREMENT,
+	"email"	TEXT
+)""")
 
 tree = ET.parse('data.xml')
 root = tree.getroot()
@@ -22,12 +25,17 @@ for elem in root:
 emails = list(dict.fromkeys(emails))
 emaillist = []
 for email in emails:
-    emaillist.append(email.lower())
+	if not len(email) > 60 and not email.endswith(".png") and not email.endswith(".jpg") and not email.endswith(".jpeg") and not email.endswith(".js"):
+		emaillist.append(email.lower())
 
 emailslist = list(dict.fromkeys(emaillist))
 
+print("Total Email : " + str(len(emailslist)))
+
+i = 1
 for email in emailslist:
-    sql = "INSERT INTO emails (emailid) VALUES ('" + email +"')"
+    sql = "INSERT INTO emails (email) VALUES ('" + email +"')"
     curr.execute(sql)
     conn.commit()
-    print("email added")
+    print(str(i) + " email added to database")
+    i += 1
